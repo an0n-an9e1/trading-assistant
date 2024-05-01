0186746d5c79737f4234af88ad16535731a903e6
angel <89686990+an0n-an9e1@users.noreply.github.com>
2024-05-01 23:52:15 +0200

    Functioning categories i guess

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import LangOption from "@/ui/lang";
 import Menu from "@/ui/menu";
 import Option from "@/ui/option";
+import Category from "@/ui/category";
 
 export default function Home() {
   return (
     <div>
       {/* Nav */}
-      <div className="flex justify-between items-center m-4 my-8">
+      <div className="flex justify-between items-center m-4 my-6">
         <div className="w-14 h-14 bg-[#16253B] rounded-xl"></div>
         <a className="font-extrabold underline text-[#16253B]">Ask for Bill</a>
         <LangOption active={"EN"} /> 
@@ -21,27 +22,10 @@ export default function Home() {
       </div>
 
       {/* Tags */}
-      <div className="flex items-center overflow-x-clip">
-        <div className="rounded-full ml-4" style={{boxShadow: "0 5px 5px 0 rgba(0, 0, 0, 25%)"}}>
-        <div className="w-32 py-2 bg-[#16253B] rounded-full" style={{boxShadow: "0px 20px 30px 0 rgba(22, 37, 59, 0.25)"}}>
-          <p className="text-white text-center text-sm">Drinks</p>
-        </div>
-        </div>
-
-        <div className="rounded-full ml-4" style={{boxShadow: "0 5px 5px 0 rgba(0, 0, 0, 25%)"}}>
-        <div className="w-32 py-2 rounded-full" style={{boxShadow: "0px 20px 30px 0 rgba(22, 37, 59, 0.25)"}}>
-          <p className="text-center text-sm">Coffe</p>
-        </div>
-        </div>
-        <div className="rounded-full ml-4" style={{boxShadow: "0 5px 5px 0 rgba(0, 0, 0, 25%)"}}>
-        <div className="w-32 py-2 rounded-full" style={{boxShadow: "0px 20px 30px 0 rgba(22, 37, 59, 0.25)"}}>
-          <p className="text-center text-sm">Tea</p>
-        </div>
-        </div>
-      </div>
+      <Category categories={["Drinks", "Coffe", "Tea"]}/>
 
       {/* Content */}
-      <h2 className="text-[#323643] mt-6 ml-4 text-lg font-bold">Featured foods</h2>
+      <h2 className="text-[#323643] mt-[-1.5rem] ml-4 text-lg font-bold">Featured foods</h2>
 
       <div className="grid grid-cols-2 gap-4 p-4">
         <Option name="ESSPRESSO" price={50} />
---
+++ ui/category.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { useState } from "react";
+
+interface Props {
+  categories: string[]
+}
+
+export default function Category(props: Props) {
+  const [selectedIndex, setSelected] = useState<number>(0);
+
+  return (
+      <div className="flex items-start overflow-x-scroll h-20 scroller">
+      {props.categories.map((category, index) => 
+        (
+          <button key={index} onClick={() => setSelected(index)} className="rounded-full ml-4" style={{boxShadow: "0 5px 5px 0 rgba(0, 0, 0, 25%)"}}>
+          <div className="w-32 py-2 rounded-full" style={{boxShadow: "0px 20px 30px 0 rgba(22, 37, 59, 0.25)", background: index === selectedIndex ? "#16253B" : "white"}}>
+            <p className="text-center text-sm" style={{color: index === selectedIndex ? "white" : "black", fontWeight: "500"}}>Drinks</p>
+          </div>
+          </button>    
+        )
+      )}
+      </div>
+  ) 
+}
